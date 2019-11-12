7dd44014fe9ef4d26c465778e7afc57ac4560922
tskon <tsekhmeister.k@gmail.com>
2019-11-13 02:03:26 +0300

    complete

--- js/custom-select.js
+++ js/custom-select.js
@@ -2,7 +2,8 @@ function initCustomSelects(selector = '.js-custom-select') {
   const SELECT_CLASS = 'custom-select'
   const SELECT_TRIGGER_CLASS = 'custom-select-trigger'
   const OPTION_LIST_CLASS = 'custom-select-list'
-  const OPTION_LIST_HIDDEN_CLASS = 'custom-select-list-hidden'
+  const OPTION_LIST_SHOW_CLASS = 'show'
+  const SELECT_OPTION_CLASS = 'custom-select-option'
 
   const selectorList = [...document.querySelectorAll(selector)]
   selectorList.forEach(initCustomSelect)
@@ -13,12 +14,15 @@ function initCustomSelects(selector = '.js-custom-select') {
     const customSelect = document.createElement('div')
     customSelect.className = SELECT_CLASS
     const optionList = document.createElement('div')
-    optionList.className = OPTION_LIST_CLASS + ' ' + OPTION_LIST_HIDDEN_CLASS
+    optionList.className = OPTION_LIST_CLASS
 
     const trigger = renderTrigger(parsedOptionList[0])
     trigger.addEventListener('click', (e) => {
       e.stopPropagation()
-      optionList.classList.toggle(OPTION_LIST_HIDDEN_CLASS)
+      customSelect.classList.toggle(OPTION_LIST_SHOW_CLASS)
+      const activeEl = document.querySelector(`.${SELECT_OPTION_CLASS}[data-value="${select.value}"]`)
+      activeEl.focus()
+      window.addEventListener('keydown', keyHandler)
       window.addEventListener('click', hideOptionList)
     })
     customSelect.appendChild(trigger)
@@ -28,7 +32,7 @@ function initCustomSelects(selector = '.js-custom-select') {
       newOption.addEventListener('click', () => {
         select.value = option.value
         trigger.innerHTML = getOptionContent(option)
-        optionList.classList.add(OPTION_LIST_HIDDEN_CLASS)
+        customSelect.classList.remove(OPTION_LIST_SHOW_CLASS)
         window.removeEventListener('click', hideOptionList)
       })
       optionList.appendChild(newOption)
@@ -38,10 +42,34 @@ function initCustomSelects(selector = '.js-custom-select') {
     select.parentNode.insertBefore(customSelect, select)
 
     const selectWidth = customSelect.querySelector('.' + OPTION_LIST_CLASS).clientWidth
-    customSelect.style.width = selectWidth + 'px'
+    customSelect.style.width = (selectWidth + 2) + 'px' // 2px - border
 
     function hideOptionList () {
-      optionList.classList.add(OPTION_LIST_HIDDEN_CLASS)
+      customSelect.classList.remove(OPTION_LIST_SHOW_CLASS)
+      window.removeEventListener('keydown', keyHandler)
+    }
+
+    function keyHandler (e) {
+      if (e.code === 'Tab' || e.code === 'ArrowUp' || e.code === 'ArrowDown') {
+        e.preventDefault()
+      } else {
+        return
+      }
+
+      const selectors = (e.code === 'Tab') ? 'button, a, input, textarea' : '.' + SELECT_OPTION_CLASS
+      const selectableElements = [...optionList.querySelectorAll(selectors)]
+      let focusedElIndex = selectableElements.findIndex(el => el === document.activeElement)
+      if (focusedElIndex === -1) focusedElIndex = 0
+
+      if (e.code === 'ArrowDown' || (e.code === 'Tab' && !e.shiftKey)) {
+        focusedElIndex = (focusedElIndex === selectableElements.length - 1) ? 0 : focusedElIndex + 1
+      }
+
+      if (e.code === 'ArrowUp' || (e.code === 'Tab' && e.shiftKey)) {
+        focusedElIndex = (focusedElIndex === 0) ? selectableElements.length - 1 : focusedElIndex - 1
+      }
+
+      selectableElements[focusedElIndex].focus()
     }
   }
 
@@ -82,8 +110,9 @@ function initCustomSelects(selector = '.js-custom-select') {
   function renderOption(parsedOption) {
     const option = document.createElement('button')
     option.type = 'button'
-    option.className = 'custom-select-option'
+    option.className = SELECT_OPTION_CLASS
     option.innerHTML = getOptionContent(parsedOption)
+    option.dataset.value = parsedOption.value
 
     return option
   }
